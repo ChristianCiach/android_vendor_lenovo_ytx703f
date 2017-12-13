8b11cae1a0a0164850bf2d28921904ad060aada3
Christian Ciach <christian.ciach@gmail.com>
2017-12-13 14:58:15 +0100

    vendor tree for files specific to YT-X703F

---
+++ YTX703F-vendor.mk
@@ -0,0 +1,57 @@
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+PRODUCT_COPY_FILES += \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/a530_pfp.fw:system/etc/firmware/a530_pfp.fw \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/a530_pm4.fw:system/etc/firmware/a530_pm4.fw \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/bdwlan30.bin:system/etc/firmware/bdwlan30.bin \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/bdwlan30_R01.bin:system/etc/firmware/bdwlan30_R01.bin \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/btfw32.tlv:system/etc/firmware/btfw32.tlv \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/btnv32.bin:system/etc/firmware/btnv32.bin \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/cpp_firmware_v1_10_0.fw:system/etc/firmware/cpp_firmware_v1_10_0.fw \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/cpp_firmware_v1_1_1.fw:system/etc/firmware/cpp_firmware_v1_1_1.fw \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/cpp_firmware_v1_1_6.fw:system/etc/firmware/cpp_firmware_v1_1_6.fw \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/cpp_firmware_v1_2_0.fw:system/etc/firmware/cpp_firmware_v1_2_0.fw \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/cpp_firmware_v1_4_0.fw:system/etc/firmware/cpp_firmware_v1_4_0.fw \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/cpp_firmware_v1_5_0.fw:system/etc/firmware/cpp_firmware_v1_5_0.fw \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/cpp_firmware_v1_6_0.fw:system/etc/firmware/cpp_firmware_v1_6_0.fw \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/cpp_firmware_v1_8_0.fw:system/etc/firmware/cpp_firmware_v1_8_0.fw \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus.b00:system/etc/firmware/venus.b00 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus.b01:system/etc/firmware/venus.b01 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus.b02:system/etc/firmware/venus.b02 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus.b03:system/etc/firmware/venus.b03 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus.b04:system/etc/firmware/venus.b04 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus.mbn:system/etc/firmware/venus.mbn \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus.mdt:system/etc/firmware/venus.mdt \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus-v1.b00:system/etc/firmware/venus-v1.b00 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus-v1.b01:system/etc/firmware/venus-v1.b01 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus-v1.b02:system/etc/firmware/venus-v1.b02 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus-v1.b03:system/etc/firmware/venus-v1.b03 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus-v1.b04:system/etc/firmware/venus-v1.b04 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus-v1.mbn:system/etc/firmware/venus-v1.mbn \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/venus-v1.mdt:system/etc/firmware/venus-v1.mdt \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/widevine.b00:system/etc/firmware/widevine.b00 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/widevine.b01:system/etc/firmware/widevine.b01 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/widevine.b02:system/etc/firmware/widevine.b02 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/widevine.b03:system/etc/firmware/widevine.b03 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/widevine.b04:system/etc/firmware/widevine.b04 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/widevine.b05:system/etc/firmware/widevine.b05 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/widevine.b06:system/etc/firmware/widevine.b06 \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/widevine.mdt:system/etc/firmware/widevine.mdt \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/wlan/prima/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
+    vendor/lenovo/YTX703F/proprietary/etc/firmware/wlan/prima/WCNSS_wlan_dictionary.dat:system/etc/firmware/wlan/prima/WCNSS_wlan_dictionary.dat \
